67a42fbf4e91e2dd1c0480f85b07074a7ad1d4bf
JeanBaptisteCheramy <jeanbaptiste.cheramy@gmail.com>
2026-01-24 18:27:43 +0100

    ajout elevated button pour ReceiptsScreen + couleurs de theme disabled

--- lib/components/screens/ReceiptsScreen/ReceiptsScreen.dart
+++ lib/components/screens/ReceiptsScreen/ReceiptsScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quittance_app/main.dart';
 
 import '../../common/AppBar/CustomAppBar.dart';
 import '../../common/CustomBottomNavigation.dart';
@@ -9,10 +10,26 @@ class ReceiptsScreen extends StatefulWidget {
 }
 
 class _ReceiptsScreenState extends State<ReceiptsScreen> {
+  bool isButtonEnabled = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: "Quittances"),
+      floatingActionButton: FloatingActionButton(
+        onPressed: isButtonEnabled ? () {} : null,
+        backgroundColor: isButtonEnabled
+            ? Theme.of(context).colorScheme.primary
+            : Theme.of(context).colorScheme.disabled,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(22)),
+        child: Icon(
+          Icons.add_rounded,
+          color: isButtonEnabled
+              ? Theme.of(context).colorScheme.onPrimary
+              : Theme.of(context).colorScheme.onDisabled,
+          size: 40,
+        ),
+      ),
       bottomNavigationBar: CustomBottomNavigationBar(),
     );
   }
